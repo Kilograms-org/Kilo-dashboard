b061b5ca1ea0c24d1be67841a3ec439525c48a48
0xtusharganotra <ganotra.vox@gmail.com>
2025-08-21 10:32:46 +0530

    fix: remove productpreviewpage import

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -9,7 +9,6 @@ import OrderDetails from "./Orderdetails";
 import CreateProduct from "./createproduct";
 import DeleteProduct from "./deleteproduct";
 import UpdateProduct from "./updateproduct";
-import ProductPreviewPage from "./productpreview";
 
 function Dashboard() {
   const {
